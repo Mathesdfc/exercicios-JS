bfc509f766e1be6221809403c00b279fc1e5bef3
mathesdfc@hotmail.com <mathesdfc@hotmail.com>
2023-01-18 23:49:02 -0300

    atualizando exercicios

---
+++ aula01-Basico/index.js
@@ -0,0 +1,18 @@
+/*
+
+- Monte um script em um arquivo com nome de aula1_js.html, que calcule a soma a de 10 + 5 e devolva seu resultado através do console em formato numérico.
+
+- Receba um valor através do comando prompt no mesmo arquivo onde criou o seu primeiro cálculo, e exiba este valor no console do Google Chrome.
+
+*/
+
+
+let Soma = 10 + 5;
+
+console.log('A soma é: ',Soma);
+
+let recebeValor = Number(prompt('Digite algum valor'));
+    
+console.log('Valor digitado: ', recebeValor);
+
+
